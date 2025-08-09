c0260de1e7cebb544d93a3625dbd1aed52ffd300
Artem Shvachov <shvachev91@gmail.com>
2025-08-09 15:41:09 +0300

    add products router

---
+++ src/routers/products.js
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+export const productsRouter = Router();
+
+productsRouter.get('/', () => {});
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { env } from './utils/env.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { productsRouter } from './routers/products.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -13,7 +14,7 @@ export const setupServer = () => {
   app.use(express.json());
   app.use(cors());
 
-  //   app.use(productsRouter);
+  app.use('/products', productsRouter);
   //   app.use(authRouter);
   //   app.use(usersRouter);
 
